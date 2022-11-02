6a60f29c014ef30852ee32ae4a546828afbfb9ab
emily montoya <e_montoya@u.pacific.edu>
2022-11-02 14:14:45 -0700

    Added toString() to blockType class

--- src/main/java/edu/pacific/comp55/ProjectCode/BlockType.java
+++ src/main/java/edu/pacific/comp55/ProjectCode/BlockType.java
@@ -1,9 +1,18 @@
 package edu.pacific.comp55.ProjectCode;
 
 public enum BlockType {
-
 	RIGHTL, LEFTL, T, RIGHTS, LEFTS, SQUARE, BAR;
 	
-	
-	
+	public String toString() {
+		switch(this) {
+			case RIGHTL: return "Right L";
+			case LEFTL: return "Left L";
+			case T: return "T";
+			case RIGHTS: return "Right S";
+			case LEFTS: return "Left S";
+			case SQUARE: return "Square";
+			case BAR: return "Bar";
+		}
+		return "n/a";
+	}
 }
