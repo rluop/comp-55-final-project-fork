a31ebf8b7a346beb5d8007e311a688006c9beade
Amr Mahmoud <a_mahmoud2@u.pacific.edu>
2022-11-02 14:26:16 -0700

    Finished Orientation enum class

--- src/main/java/edu/pacific/comp55/ProjectCode/Orientation.java
+++ src/main/java/edu/pacific/comp55/ProjectCode/Orientation.java
@@ -1,5 +1,15 @@
 package edu.pacific.comp55.ProjectCode;
 
-public class Orientation {
-
+public enum Orientation {
+	UP, DOWN, LEFT, RIGHT;
+	
+	public String toString() {
+		switch(this) {
+			case UP: return "Up";
+			case DOWN: return "Down";
+			case LEFT: return "Left";
+			case RIGHT: return "Right";
+		}
+		return "n/a";
+	}
 }
