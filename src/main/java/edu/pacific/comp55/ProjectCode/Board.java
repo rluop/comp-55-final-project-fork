75fca85fa501e5d607675517c183de3cb80d6599
emily montoya <e_montoya@u.pacific.edu>
2022-11-02 14:53:24 -0700

    added Block array in Board class

--- src/main/java/edu/pacific/comp55/ProjectCode/Board.java
+++ src/main/java/edu/pacific/comp55/ProjectCode/Board.java
@@ -1,6 +1,11 @@
 package edu.pacific.comp55.ProjectCode;
 
+import java.util.*;
+
 public class Board {
 	private int numRows;
 	private int numCols;
+	
+	//Block[] list; make a list instead like a vector to input easier
+	private ArrayList<Block>block = new ArrayList<Block>();
 }
