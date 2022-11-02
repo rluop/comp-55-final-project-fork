ac42479eb0a3184c013e51ebc7acf9cfb02d36e9
nora-bakken <n_bakken@u.pacific.edu>
2022-11-02 14:35:08 -0700

    created first base for GraphicsGame

--- src/main/java/edu/pacific/comp55/ProjectCode/GraphicsGame.java
+++ src/main/java/edu/pacific/comp55/ProjectCode/GraphicsGame.java
@@ -1,5 +1,20 @@
 package edu.pacific.comp55.ProjectCode;
 
-public class GraphicsGame {
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
 
+public class GraphicsGame extends GraphicsProgram {
+	private Timer t;
+	
+	public void init() {
+		//needed for graphics program
+	}
+	
+	public void run() {
+		//needed for graphics program
+	}
 }
