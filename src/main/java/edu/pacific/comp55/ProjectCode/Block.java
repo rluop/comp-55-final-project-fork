3faedf7ed917c2b2dda7746b923df9d65efdb7b9
Amr Mahmoud <a_mahmoud2@u.pacific.edu>
2022-11-16 14:59:57 -0800

    Finished spacesOccupied method in Block.java

--- src/main/java/edu/pacific/comp55/ProjectCode/Block.java
+++ src/main/java/edu/pacific/comp55/ProjectCode/Block.java
@@ -46,58 +46,22 @@ public class Block {
 	}
 	
 	boolean canRotateType() {
-		//nora
-		//there should be another version of this in board that checks if it can rotate with respect to the other blocks on the board
-		//this function jsust checks if we're in a phase that allows rotation (block isn't a rock etc)
 		if(!rock && !rotationPhase) {
 			return true;
 		}
 		return false;
 	}
 	
-	boolean canMoveDown() {
-		//nora
-		Space[] returnMe = spacesOccupied();
-		for(int i = 0; i<returnMe.length; i++) {
-			if(returnMe[i].getRow()-1 < 0) {
-				System.out.println("move out of bounds!");
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	boolean canMoveLeft() {
+	Space[] spacesIfMoveLeft() { //return an array of the spaces, check if empty in the BOARD class - prof suggestion
+		//nora: i think this should send an array of the spaces a block would occupy to canPlaceBlock in board, and then we can decide if the block should be placed or not somewhere else
+		//so rn i THINK this should just take spaces occupied and change a row, but please lmk if you think it should do something else!
 		Space[] returnMe = spacesOccupied();
 		for(int i = 0; i<returnMe.length; i++) {
 			//each col -1 when we move left, check to make sure its not out of bounds
 			if(returnMe[i].getCol()-1 < 0) {
 				System.out.println("move out of bounds!");
-				return false;
+				return returnMe;
 			}
-		}
-		return true;
-	}
-	
-	boolean canMoveRight() {
-		//nora
-		Space[] returnMe = spacesOccupied();
-		for(int i = 0; i<returnMe.length; i++) {
-			//each col +1 when we move right, bounds check first
-			if(returnMe[i].getCol()+1 > 10) {
-				System.out.println("move out of bounds!");
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	Space[] spacesIfMoveLeft() { //return an array of the spaces, check if empty in the BOARD class - prof suggestion
-		//nora: i think this should send an array of the spaces a block would occupy to canPlaceBlock in board, and then we can decide if the block should be placed or not somewhere else
-		//so rn i THINK this should just take spaces occupied and change a row, but please lmk if you think it should do something else!
-		Space[] returnMe = spacesOccupied();
-		if(!canMoveLeft()) {
-			return returnMe;
 		}
 		for(int i = 0; i<returnMe.length; i++) {
 			//must have passed the checks, now set it
@@ -109,8 +73,12 @@ public class Block {
 	Space[] spacesIfMoveRight() {
 		//nora
 		Space[] returnMe = spacesOccupied();
-		if(!canMoveRight()) {
-			return returnMe;
+		for(int i = 0; i<returnMe.length; i++) {
+			//each col +1 when we move right, bounds check first
+			if(returnMe[i].getCol()+1 > 10) {
+				System.out.println("move out of bounds!");
+				return returnMe;
+			}
 		}
 		//assuming it passed the bounds check
 		for(int i = 0; i<returnMe.length; i++) {
@@ -119,12 +87,15 @@ public class Block {
 		return returnMe;
 	}
 	
-	Space[] spacesIfMoveDown() {//
+	Space[] spacesIfMoveDown() {
 		//nora
 		//dont forget we need a check in board to see if theres already a block in the spaces this function returns. this function only returns where the block WOULD be, not if the block can be there
 		Space[] returnMe = spacesOccupied();
-		if(!canMoveDown()) {
-			return returnMe;
+		for(int i = 0; i<returnMe.length; i++) {
+			if(returnMe[i].getRow()-1 < 0) {
+				System.out.println("move out of bounds!");
+				return returnMe;
+			}
 		}
 		for(int i = 0; i<returnMe.length; i++) {
 			returnMe[i].setRow(returnMe[i].getRow()-1);
@@ -136,9 +107,14 @@ public class Block {
 		return this.orientation;
 	}
 	
-	void rotate() {
+	boolean canRotate() {
 		//nora
-		
+		//i believe this needs to go in board. ill look into it more later
+		return true;
+	}
+	
+	void rotate() {
+		//
 	}
 	
 	public Space[] spacesOccupied() {
@@ -146,93 +122,134 @@ public class Block {
 		if (this.type == BlockType.RIGHTL) {
 			if (this.orientation == Orientation.UP) { 
 				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
+				spaceArray[2] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() - 2);
+				spaceArray[3] = new Space(spaceArray[0].getRow() - 1, spaceArray[0].getCol());
+				
+			}
+			else if (this.orientation == Orientation.RIGHT) { 
+				spaceArray[0] = this.startSpace;
 				spaceArray[1] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol());
 				spaceArray[2] = new Space(spaceArray[0].getRow() - 2, spaceArray[0].getCol());
 				spaceArray[3] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() + 1);
 			}
-			else if (this.orientation == Orientation.RIGHT) { 
+			else if (this.orientation == Orientation.DOWN) { 
 				spaceArray[0] = this.startSpace;
 				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
 				spaceArray[2] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() + 2);
 				spaceArray[3] = new Space(spaceArray[0].getRow() + 1, spaceArray[0].getCol());
 			}
-			else if (this.orientation == Orientation.DOWN) { 
+			else {
 				spaceArray[0] = this.startSpace;
 				spaceArray[1] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
 				spaceArray[2] = new Space(spaceArray[0].getRow() + 2, spaceArray[0].getCol());
 				spaceArray[3] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() - 1);
-			}
-			else {
-				spaceArray[0] = this.startSpace;
-				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
-				spaceArray[2] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() - 2);
-				spaceArray[3] = new Space(spaceArray[0].getRow() - 1, spaceArray[0].getCol());
 			}
 		}
 		else if (this.type == BlockType.LEFTL) {
 			if (this.orientation == Orientation.UP) {
 				spaceArray[0] = this.startSpace;
-				spaceArray[1] = new Space(spaceArray[0].getRow() - 1, spaceArray[0].getCol());
-				spaceArray[2] = new Space(spaceArray[0].getRow() - 2, spaceArray[0].getCol());
-				spaceArray[3] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() - 1);
-			}
-			else if (this.orientation == Orientation.RIGHT) { 
-				spaceArray[0] = this.startSpace;
 				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
 				spaceArray[2] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() + 2);
 				spaceArray[3] = new Space(spaceArray[0].getRow() - 1, spaceArray[0].getCol());
 			}
-			else if (this.orientation == Orientation.DOWN) { 
+			else if (this.orientation == Orientation.RIGHT) { 
 				spaceArray[0] = this.startSpace;
 				spaceArray[1] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
 				spaceArray[2] = new Space(spaceArray[0].getRow() + 2, spaceArray[0].getCol());
 				spaceArray[3] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() + 1);
 			}
-			else {
+			else if (this.orientation == Orientation.DOWN) { 
 				spaceArray[0] = this.startSpace;
 				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
 				spaceArray[2] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() - 2);
 				spaceArray[3] = new Space(spaceArray[0].getRow() + 1, spaceArray[0].getCol());
+			}
+			else {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow() - 1, spaceArray[0].getCol());
+				spaceArray[2] = new Space(spaceArray[0].getRow() - 2, spaceArray[0].getCol());
+				spaceArray[3] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() - 1);
 			}
 			
 		}
 		else if (this.type == BlockType.T) {
 			if (this.orientation == Orientation.UP) {
 				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
+				spaceArray[2] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() - 1);
+				spaceArray[3] = new Space(spaceArray[0].getRow() - 1, spaceArray[0].getCol());
+			}
+			else if (this.orientation == Orientation.RIGHT) { 
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
+				spaceArray[2] = new Space(spaceArray[0].getRow() - 1, spaceArray[0].getCol());
+				spaceArray[3] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() + 1);
+			}
+			else if (this.orientation == Orientation.DOWN) { 
+				spaceArray[0] = this.startSpace;
 				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
 				spaceArray[2] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
 				spaceArray[3] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
 			}
-			else if (this.orientation == Orientation.RIGHT) { 
+			else {
 				spaceArray[0] = this.startSpace;
 				spaceArray[1] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol());
 				spaceArray[2] = new Space(spaceArray[0].getRow() + 1, spaceArray[0].getCol());
 				spaceArray[3] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() - 1);
 			}
-			else if (this.orientation == Orientation.DOWN) { 
+		}
+		else if (this.type == BlockType.RIGHTS) {
+			if (this.orientation == Orientation.UP) {
 				spaceArray[0] = this.startSpace;
 				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
-				spaceArray[2] = new Space(spaceArray[0].getRow(), spaceArray[0].getCol() - 1);
-				spaceArray[3] = new Space(spaceArray[0].getRow() - 1, spaceArray[0].getCol());
+				spaceArray[2] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
+				spaceArray[3] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol() - 1);
+			}
+			else if (this.orientation == Orientation.RIGHT) {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
+				spaceArray[2] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
+				spaceArray[3] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol() - 1);
+			}
+			else if (this.orientation == Orientation.DOWN) {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
+				spaceArray[2] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol());
+				spaceArray[3] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol() + 1);
+			}
+			else {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol());
+				spaceArray[2] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
+				spaceArray[3] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol() + 1);
+			}
+		}
+		else if (this.type == BlockType.LEFTS) {
+			if (this.orientation == Orientation.UP) {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
+				spaceArray[2] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
+				spaceArray[3] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol() + 1);
+			}
+			else if (this.orientation == Orientation.RIGHT) {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol());
+				spaceArray[2] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
+				spaceArray[3] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol() - 1);
+			}
+			else if (this.orientation == Orientation.DOWN) {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
+				spaceArray[2] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol());
+				spaceArray[3] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol() - 1);
 			}
 			else {
 				spaceArray[0] = this.startSpace;
 				spaceArray[1] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
-				spaceArray[2] = new Space(spaceArray[0].getRow() - 1, spaceArray[0].getCol());
-				spaceArray[3] = new Space(spaceArray[0].getRow() + 1, spaceArray[0].getCol() + 1);
+				spaceArray[2] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
+				spaceArray[3] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol() + 1);
 			}
-		}
-		else if (this.type == BlockType.RIGHTS) {
-			spaceArray[0] = this.startSpace;
-			spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
-			spaceArray[2] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
-			spaceArray[3] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol() - 1);
-		}
-		else if (this.type == BlockType.LEFTS) {
-			spaceArray[0] = this.startSpace;
-			spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
-			spaceArray[2] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
-			spaceArray[3] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol() + 1);
 		}
 		else if (this.type == BlockType.SQUARE) {
 			spaceArray[0] = this.startSpace;
@@ -241,10 +258,30 @@ public class Block {
 			spaceArray[3] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol() + 1);
 		}
 		else {
-			spaceArray[0] = this.startSpace;
-			spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
-			spaceArray[2] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 2);
-			spaceArray[3] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 3);
+			if (this.orientation == Orientation.UP) {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
+				spaceArray[2] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 2);
+				spaceArray[3] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
+			}
+			else if (this.orientation == Orientation.RIGHT) {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
+				spaceArray[2] = new Space(spaceArray[0].getRow() + 2,spaceArray[0].getCol());
+				spaceArray[3] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol());
+			}
+			else if (this.orientation == Orientation.DOWN) {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 1);
+				spaceArray[2] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() - 2);
+				spaceArray[3] = new Space(spaceArray[0].getRow(),spaceArray[0].getCol() + 1);
+			}
+			else {
+				spaceArray[0] = this.startSpace;
+				spaceArray[1] = new Space(spaceArray[0].getRow() - 1,spaceArray[0].getCol());
+				spaceArray[2] = new Space(spaceArray[0].getRow() - 2,spaceArray[0].getCol());
+				spaceArray[3] = new Space(spaceArray[0].getRow() + 1,spaceArray[0].getCol());
+			}
 		}
 		return spaceArray;
 	}
