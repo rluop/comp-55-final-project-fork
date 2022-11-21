c35f8af46ededd65bacfb416bd5bb19f9d27978b
emily montoya <e_montoya@u.pacific.edu>
2022-11-21 14:38:19 -0800

    changed variable name from img to controls

--- src/main/java/edu/pacific/comp55/starter/SomePane.java
+++ src/main/java/edu/pacific/comp55/starter/SomePane.java
@@ -7,25 +7,25 @@ import acm.graphics.GObject;
 public class SomePane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
-	private GImage img;
+	private GImage controls;
 	private GParagraph para;
 
 	public SomePane(MainApplication app) {
 		this.program = app;
-		img = new GImage("controls.png", 10, 10);
+		controls = new GImage("controls.png", 10, 10);
 		para = new GParagraph("welcome\nto my\nsecret room!", 150, 300);
 		para.setFont("Arial-24");
 	}
 
 	@Override
 	public void showContents() {
-		program.add(img);
+		program.add(controls);
 		program.add(para);
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(img);
+		program.remove(controls);
 		program.remove(para);
 	}
 
@@ -33,7 +33,7 @@ public class SomePane extends GraphicsPane {
 	public void mousePressed(MouseEvent e) {
 		para.setText("you need\nto click\non the eyes\nto go back");
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == img) {
+		if (obj == controls) {
 			program.switchToMenu();
 		}
 	}
