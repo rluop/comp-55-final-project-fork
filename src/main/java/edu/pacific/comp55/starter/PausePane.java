8360b840fc6164fd93edbd6868512365cd5472b2
emily montoya <e_montoya@u.pacific.edu>
2022-11-24 13:47:40 -0800

    moved pause png to new folder

--- src/main/java/edu/pacific/comp55/starter/PausePane.java
+++ src/main/java/edu/pacific/comp55/starter/PausePane.java
@@ -7,12 +7,12 @@ import acm.graphics.GObject;
 public class PausePane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
-	private GImage controls;
+	private GImage pause;
 	//private GParagraph para;
 
 	public PausePane(MainApplication app) {
 		this.program = app;
-		controls = new GImage("controls.png", 0, 0);
+		pause = new GImage("controls.png", 0, 0);
 		//para = new GParagraph("welcome\nto my\nsecret room!", 150, 300);
 		//para.setFont("Arial-24");
 		
@@ -21,14 +21,14 @@ public class PausePane extends GraphicsPane {
 
 	@Override
 	public void showContents() {
-		program.add(controls);
+		//program.add(controls);
 		
 		//program.add(para);
 	}
 
 	@Override
 	public void hideContents() {
-		program.remove(controls);
+		//program.remove(controls);
 		//program.remove(para);
 	}
 
@@ -36,7 +36,7 @@ public class PausePane extends GraphicsPane {
 	public void mousePressed(MouseEvent e) {
 		//para.setText("you need\nto click\non the eyes\nto go back");
 		GObject obj = program.getElementAt(e.getX(), e.getY());
-		if (obj == controls) {
+		if (obj == pause) {
 			program.switchToMenu();
 		}
 	}
