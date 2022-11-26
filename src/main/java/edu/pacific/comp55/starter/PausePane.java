8d9a867d426912ec4ea4597cf8d902f37b300a59
emily montoya <e_montoya@u.pacific.edu>
2022-11-26 13:17:04 -0800

    created two buttons for pause page

--- src/main/java/edu/pacific/comp55/starter/PausePane.java
+++ src/main/java/edu/pacific/comp55/starter/PausePane.java
@@ -1,4 +1,5 @@
 package edu.pacific.comp55.starter;
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GImage;
@@ -8,10 +9,20 @@ public class PausePane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
 	private GImage pause;
+	private GButton resume;
+	private GButton menu;
 
 	public PausePane(MainApplication app) {
 		this.program = app;
 		pause = new GImage("pause.png", 0, 0);
+		
+		resume = new GButton("controls", 610, 610, 300, 120);
+		resume.setColor(Color.black);
+		//resume.setVisible(false);
+		
+		menu = new GButton("controls", 610, 610, 300, 120);
+		menu.setColor(Color.red);
+		//menu.setVisible(false);
 	}
 
 	@Override
