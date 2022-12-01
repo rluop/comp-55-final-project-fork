44a7a9e16121d4fb1431706170b000afda4b7bd8
nora-bakken <n_bakken@u.pacific.edu>
2022-11-30 22:35:51 -0800

    moved functions that require keylisteners to the key listener declaration in main

--- src/main/java/edu/pacific/comp55/ProjectCode/Game.java
+++ src/main/java/edu/pacific/comp55/ProjectCode/Game.java
@@ -11,7 +11,7 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 
 public class Game {
-	private Board board;
+	private static Board board;
 	
 	public static void main(String[] args) {
 		Game g = new Game();
@@ -36,6 +36,72 @@ public class Game {
 			public void keyReleased(KeyEvent e) {
 				System.out.println("key released");
 				// TODO Auto-generated method stub
+			}
+			public void moveDown(KeyEvent e) {
+				// check if moveActiveBlockDown is true in Board
+				if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+					System.out.println("DOWN");
+					board.moveActiveBlockDown();
+					// activeBlock.move(0, 50);
+				}
+				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+					// check spacesOccupied in Block
+					// activeBlock.setLocation(activeBlock.getX(), ?);
+				}
+			}
+			
+			public void moveHorizontal(KeyEvent e) {
+				// check if moveActiveBlockLeft is true in Board
+				if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+					System.out.println("LEFT");
+					board.moveActiveBlockLeft();
+					// activeBlock.move(-50, 0);
+				}
+				// check if moveActiveBlockRight is true in Board
+				if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+					System.out.println("RIGHT");
+					board.moveActiveBlockRight();
+					// activeBlock.move(50, 0);
+				}
+			}
+			
+			public void holdBlock(KeyEvent e) {
+				// check if canHold is true in Board
+				if (e.getKeyCode() == KeyEvent.VK_C) {
+					// TODO
+				}
+			}
+			
+			public void rotate(KeyEvent e) {	
+				// check if canRotate is true in Board
+				// int rotationNum = 0;
+				if (board.canRotate()) {
+					if (e.getKeyCode() == KeyEvent.VK_UP) {
+						board.rotateBlockRight();
+						/*
+						if (rotationNum == 3) {
+							rotationNum = 0;
+						}
+						else {
+							rotationNum++;
+							spaceArray(rotationNum);
+						}
+
+						*/
+					}
+					if (e.getKeyCode() == KeyEvent.VK_Z) {
+						board.rotateBlockRight();
+						/*
+						if (rotationNum == 0) {
+							rotationNum = 3;
+						}
+						else {
+							rotationNum--;
+							spaceArray(rotationNum);
+						}
+						*/
+					}
+				}
 			}
 		};
 		JTextField textField = new JTextField();
@@ -62,73 +128,6 @@ public class Game {
 			wait(1500);
 			board.moveActiveBlockDown();
 			
-		}
-	}
-	
-	public void moveDown(KeyEvent e) {
-		// check if moveActiveBlockDown is true in Board
-		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			System.out.println("DOWN");
-			board.moveActiveBlockDown();
-			// activeBlock.move(0, 50);
-		}
-		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-			// check spacesOccupied in Block
-			// activeBlock.setLocation(activeBlock.getX(), ?);
-		}
-	}
-	
-	public void moveHorizontal(KeyEvent e) {
-		// check if moveActiveBlockLeft is true in Board
-		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			System.out.println("LEFT");
-			board.moveActiveBlockLeft();
-			// activeBlock.move(-50, 0);
-		}
-		// check if moveActiveBlockRight is true in Board
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			System.out.println("RIGHT");
-			board.moveActiveBlockRight();
-			// activeBlock.move(50, 0);
-		}
-	}
-	
-	public void holdBlock(KeyEvent e) {
-		// check if canHold is true in Board
-		if (e.getKeyCode() == KeyEvent.VK_C) {
-			// TODO
-		}
-	}
-	
-	public void rotate(KeyEvent e) {	
-		// check if canRotate is true in Board
-		// int rotationNum = 0;
-		if (board.canRotate()) {
-			if (e.getKeyCode() == KeyEvent.VK_UP) {
-				board.rotateBlockRight();
-				/*
-				if (rotationNum == 3) {
-					rotationNum = 0;
-				}
-				else {
-					rotationNum++;
-					spaceArray(rotationNum);
-				}
-
-				*/
-			}
-			if (e.getKeyCode() == KeyEvent.VK_Z) {
-				board.rotateBlockRight();
-				/*
-				if (rotationNum == 0) {
-					rotationNum = 3;
-				}
-				else {
-					rotationNum--;
-					spaceArray(rotationNum);
-				}
-				*/
-			}
 		}
 	}
 	
