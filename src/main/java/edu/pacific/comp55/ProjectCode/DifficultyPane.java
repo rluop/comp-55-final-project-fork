641d85a64491bf143c84cce8b5c332dc3e501d2c
rluop <r_lorenzo@u.pacific.edu>
2024-01-30 08:53:19 -0800

    added three difficulty buttons to game

--- src/main/java/edu/pacific/comp55/ProjectCode/DifficultyPane.java
+++ src/main/java/edu/pacific/comp55/ProjectCode/DifficultyPane.java
@@ -1,21 +1,36 @@
 package edu.pacific.comp55.ProjectCode;
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 
 import acm.graphics.GImage;
 import acm.graphics.GObject;
+import edu.pacific.comp55.starter.GButton;
 import edu.pacific.comp55.starter.GraphicsPane;
 
 public class DifficultyPane extends GraphicsPane {
 	private MainApplication program; // you will use program to get access to
 										// all of the GraphicsProgram calls
 	private GImage back;
+	private GButton normal;
+	private GButton fast;
+	private GButton faster;
 
 	public DifficultyPane(MainApplication app) {
 		this.program = app;
 		back = new GImage("difficulty.png", 0, 0);
 		back.setSize(program.WINDOW_WIDTH, program.WINDOW_HEIGHT);
 		
+		normal = new GButton("normal difficulty", 200, 200, 200, 100);
+		normal.setColor(Color.black);
+		normal.setVisible(true);
 		
+		fast = new GButton("fast difficulty", 200, 300, 200, 100);
+		fast.setColor(Color.black);
+		fast.setVisible(true);
+		
+		faster = new GButton("faster difficulty", 200, 400, 200, 100);
+		faster.setColor(Color.black);
+		faster.setVisible(true);		
 	}
 
 	@Override
